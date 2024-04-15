94ad675d0011b1b1895f97abb8a349314e4b1231
George V. <georgev22.github@gmail.com>
2024-04-15 15:10:33 +0300

    feat(providers): Add PurpurInfo and PurpurProvider classes
    - Added PurpurInfo class representing information about a PurpurMC project.
    - Added PurpurProvider class implementing Provider for downloading server jars from PurpurMC.
    - Implements methods for retrieving project name, version, and PurpurMC builds info.
    - Includes logic for downloading server jar from PurpurMC API.

---
+++ src/main/java/com/georgev22/centrojars/providers/implementations/PurpurProvider.java
@@ -0,0 +1,65 @@
+package com.georgev22.centrojars.providers.implementations;
+
+import com.georgev22.centrojars.providers.Provider;
+import com.georgev22.centrojars.providers.info.PurpurInfo;
+import com.georgev22.centrojars.utilities.Utils;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Implementation of Provider for downloading server jars from PurpurMC.
+ */
+public class PurpurProvider extends Provider {
+
+    /**
+     * Base URL of the PurpurMC API for retrieving versions.
+     */
+    private final String API_BASE_URL = "https://api.purpurmc.org/v2/purpur/";
+
+    /**
+     * Constructs a new PurpurProvider with the specified server type, implementation, and version.
+     *
+     * @param serverType           Type of the server.
+     * @param serverImplementation Name of the server implementation.
+     * @param serverVersion        Version of the server.
+     */
+    public PurpurProvider(String serverType, String serverImplementation, String serverVersion) {
+        super(serverType, serverImplementation, serverVersion);
+    }
+
+    /**
+     * Downloads the server jar from PurpurMC.
+     *
+     * @return The URL of the downloaded server jar, or null if the download fails.
+     */
+    @Override
+    public String downloadJar() {
+        String purpurAPI = API_BASE_URL + this.getServerVersion() + "/";
+        this.main.getLogger().debug("Fetching Purpur link: {}", purpurAPI);
+        try {
+            URL purpurBuildsURL = new URL(purpurAPI);
+            HttpURLConnection purpurConnection = (HttpURLConnection) purpurBuildsURL.openConnection();
+            purpurConnection.setRequestMethod("GET");
+            int purpurConnectionResponseCode = purpurConnection.getResponseCode();
+            if (purpurConnectionResponseCode == HttpURLConnection.HTTP_OK) {
+                Gson gson = new Gson();
+                PurpurInfo purpurInfo = gson.fromJson(new InputStreamReader(purpurConnection.getInputStream()), PurpurInfo.class);
+                String latest = purpurInfo.builds().latest();
+                String apiUrl = purpurAPI + latest + "/download";
+                String fileName = this.getServerVersion() + ".jar";
+                String filePath = this.main.getCentroJarsFolder() + this.getServerType() + "/" + this.getServerImplementation() + "/" + this.getServerVersion() + "/";
+                return Utils.downloadFile(apiUrl, filePath, fileName);
+            } else {
+                this.main.getLogger().error("Failed to fetch Purpur link. Response code: {} reason: {}", purpurConnectionResponseCode, purpurConnection.getResponseMessage());
+            }
+        } catch (IOException e) {
+            this.main.getLogger().error("Error fetching jar: {}", e.getMessage());
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/com/georgev22/centrojars/providers/info/PurpurInfo.java
@@ -0,0 +1,184 @@
+package com.georgev22.centrojars.providers.info;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Class representing information about a PurpurMC project.
+ */
+public final class PurpurInfo {
+    /**
+     * Project name.
+     */
+    private final String project;
+
+    /**
+     * Version of the project.
+     */
+    private final String version;
+
+    /**
+     * Information about PurpurMC builds.
+     */
+    private final PurpurBuildsInfo builds;
+
+    /**
+     * Constructs a new PurpurInfo object with the specified parameters.
+     *
+     * @param project Project name.
+     * @param version Version of the project.
+     * @param builds  Information about PurpurMC builds.
+     */
+    public PurpurInfo(String project, String version, PurpurBuildsInfo builds) {
+        this.project = project;
+        this.version = version;
+        this.builds = builds;
+    }
+
+    /**
+     * Retrieves the project name.
+     *
+     * @return The project name.
+     */
+    public String project() {
+        return project;
+    }
+
+    /**
+     * Retrieves the version of the project.
+     *
+     * @return The version of the project.
+     */
+    public String version() {
+        return version;
+    }
+
+    /**
+     * Retrieves information about PurpurMC builds.
+     *
+     * @return Information about PurpurMC builds.
+     */
+    public PurpurBuildsInfo builds() {
+        return builds;
+    }
+
+    /**
+     * Returns a string representation of the PurpurInfo object.
+     *
+     * @return A string representation of the object.
+     */
+    @Override
+    public String toString() {
+        return "PurpurInfo[" +
+                "project=" + project + ", " +
+                "version=" + version + ", " +
+                "builds=" + builds + ']';
+    }
+
+    /**
+     * Checks whether this PurpurInfo object is equal to another object.
+     *
+     * @param obj The object to compare to.
+     * @return True if the objects are equal, false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (PurpurInfo) obj;
+        return Objects.equals(this.project, that.project) &&
+                Objects.equals(this.version, that.version) &&
+                Objects.equals(this.builds, that.builds);
+    }
+
+    /**
+     * Returns the hash code value for this PurpurInfo object.
+     *
+     * @return The hash code value for the object.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(project, version, builds);
+    }
+
+    /**
+     * Class representing information about PurpurMC builds.
+     */
+    public static final class PurpurBuildsInfo {
+        /**
+         * Latest build.
+         */
+        private final String latest;
+
+        /**
+         * List of all builds.
+         */
+        private final List<String> all;
+
+        /**
+         * Constructs a new PurpurBuildsInfo object with the specified parameters.
+         *
+         * @param latest Latest build.
+         * @param all    List of all builds.
+         */
+        public PurpurBuildsInfo(String latest, List<String> all) {
+            this.latest = latest;
+            this.all = all;
+        }
+
+        /**
+         * Retrieves the latest build.
+         *
+         * @return The latest build.
+         */
+        public String latest() {
+            return latest;
+        }
+
+        /**
+         * Retrieves the list of all builds.
+         *
+         * @return The list of all builds.
+         */
+        public List<String> all() {
+            return all;
+        }
+
+        /**
+         * Returns a string representation of the PurpurBuildsInfo object.
+         *
+         * @return A string representation of the object.
+         */
+        @Override
+        public String toString() {
+            return "PurpurBuildsInfo[" +
+                    "latest=" + latest + ", " +
+                    "all=" + all + ']';
+        }
+
+        /**
+         * Checks whether this PurpurBuildsInfo object is equal to another object.
+         *
+         * @param obj The object to compare to.
+         * @return True if the objects are equal, false otherwise.
+         */
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) return true;
+            if (obj == null || obj.getClass() != this.getClass()) return false;
+            var that = (PurpurBuildsInfo) obj;
+            return Objects.equals(this.latest, that.latest) &&
+                    Objects.equals(this.all, that.all);
+        }
+
+        /**
+         * Returns the hash code value for this PurpurBuildsInfo object.
+         *
+         * @return The hash code value for the object.
+         */
+        @Override
+        public int hashCode() {
+            return Objects.hash(latest, all);
+        }
+    }
+}
