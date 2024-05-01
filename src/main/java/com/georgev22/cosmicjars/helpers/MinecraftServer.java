e6ecee3ba0101e334cbc5d9a63eb4ae7891d4d1c
George V. <georgev22.github@gmail.com>
2024-05-01 10:53:11 +0300

    feat: Add MinecraftServer helper class
    This commit adds a new helper class, `MinecraftServer`, for managing a Minecraft server process. It includes methods for starting the server, sending commands to it, and retrieving server information.

---
+++ src/main/java/com/georgev22/cosmicjars/helpers/MinecraftServer.java
@@ -0,0 +1,195 @@
+/**
+ * Helper class for managing a Minecraft server process.
+ */
+package com.georgev22.cosmicjars.helpers;
+
+import com.georgev22.cosmicjars.ConsoleFrame;
+import com.georgev22.cosmicjars.Main;
+import com.georgev22.cosmicjars.providers.Provider;
+import com.georgev22.cosmicjars.utilities.Utils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.io.*;
+import java.lang.management.ManagementFactory;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Helper class for managing a Minecraft server process.
+ */
+public class MinecraftServer {
+
+    private final Main main = Main.getInstance();
+    private final Provider provider;
+    private final File workDir;
+    private @Nullable File jarFile;
+    private final String javaExecutable;
+    private final String[] minecraftServerArguments;
+
+    private Process minecraftServerProcess;
+    private PrintWriter serverCommandWriter;
+
+    /**
+     * Constructs a MinecraftServer instance.
+     *
+     * @param provider                 The provider for the Minecraft server JAR file.
+     * @param workDir                  The working directory for the server.
+     * @param javaExecutable           The path to the Java executable.
+     * @param minecraftServerArguments The arguments to be passed to the Minecraft server.
+     */
+    public MinecraftServer(
+            @NotNull Provider provider,
+            File workDir,
+            String javaExecutable,
+            String[] minecraftServerArguments
+    ) {
+        this.provider = provider;
+        this.workDir = workDir;
+        File jarFile = new File(provider.downloadJar());
+        if (jarFile.exists()) {
+            this.jarFile = jarFile;
+        }
+        this.javaExecutable = javaExecutable;
+        this.minecraftServerArguments = minecraftServerArguments;
+    }
+
+    /**
+     * Gets the working directory for the Minecraft server.
+     *
+     * @return The working directory.
+     */
+    public File getWorkDir() {
+        return workDir;
+    }
+
+    /**
+     * Gets the JAR file for the Minecraft server.
+     *
+     * @return The JAR file.
+     */
+    public @Nullable File getJarFile() {
+        return jarFile;
+    }
+
+    /**
+     * Gets the arguments for the Minecraft server.
+     *
+     * @return The arguments array.
+     */
+    public String[] getMinecraftServerArguments() {
+        return minecraftServerArguments;
+    }
+
+    /**
+     * Gets the provider for the Minecraft server.
+     *
+     * @return The provider.
+     */
+    public Provider getProvider() {
+        return provider;
+    }
+
+    /**
+     * Gets the process representing the Minecraft server.
+     *
+     * @return The Minecraft server process.
+     */
+    public Process getMinecraftServerProcess() {
+        return minecraftServerProcess;
+    }
+
+    /**
+     * Starts the Minecraft server.
+     */
+    public void start() {
+        if (jarFile == null) {
+            this.main.getLogger().error("Jar file is null!");
+            return;
+        }
+        try {
+            List<String> vmArguments = ManagementFactory.getRuntimeMXBean().getInputArguments();
+
+            LinkedList<String> command = new LinkedList<>();
+
+            if (this.main.getProperties().getProperty("server.implementation").equalsIgnoreCase("forge")
+                    || this.main.getProperties().getProperty("server.implementation").equalsIgnoreCase("mohist")) {
+                try (FileOutputStream fos = new FileOutputStream(new File(this.workDir, "user_jvm_args.txt"))) {
+                    StringBuilder sb = new StringBuilder();
+                    for (String arg : vmArguments) {
+                        sb.append(arg).append(" ");
+                    }
+                    fos.write(sb.toString().getBytes());
+                } catch (IOException e) {
+                    this.main.getLogger().error("Failed to write user JVM arguments: {}", e.getMessage());
+                }
+                File forgeRunner = new File(this.workDir, Utils.isWindows() ? "run.bat" : "run.sh");
+                if (!forgeRunner.exists()) {
+                    this.main.getLogger().error("Could not find forge runner at: {}", forgeRunner.getAbsolutePath());
+                    Runtime.getRuntime().exit(1);
+                }
+
+                if (Utils.isWindows()) {
+                    command.add("cmd.exe");
+                    command.add("/c");
+                }
+                command.add(forgeRunner.getAbsolutePath());
+            } else {
+                command.add(this.javaExecutable);
+                command.addAll(vmArguments);
+                command.add("-jar");
+                command.add(jarFile.getAbsolutePath());
+            }
+
+            command.addAll(Arrays.stream(this.minecraftServerArguments).filter(arg -> !arg.startsWith("--cosmic")).toList());
+
+            this.main.getLogger().info("Starting Minecraft server with command: {}", String.join(" ", command));
+
+            ProcessBuilder pb = new ProcessBuilder(command);
+
+            pb.redirectErrorStream(true);
+            minecraftServerProcess = pb.start();
+            ConsoleFrame.getInstance().addCpuAndMemoryUsage(minecraftServerProcess.pid());
+
+            serverCommandWriter = new PrintWriter(minecraftServerProcess.getOutputStream());
+            InputStream processOutput = minecraftServerProcess.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(processOutput));
+
+            new Thread(() -> {
+                try {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        String finalLine = line;
+                        SwingUtilities.invokeLater(() -> ConsoleFrame.getInstance().printToConsole(finalLine));
+                    }
+                } catch (IOException e) {
+                    this.main.getLogger().error("Error reading Minecraft server output: {}", e.getMessage());
+                }
+            }).start();
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                if (minecraftServerProcess != null && minecraftServerProcess.isAlive()) {
+                    minecraftServerProcess.destroy();
+                }
+            }));
+        } catch (IOException e) {
+            this.main.getLogger().error("Error starting Minecraft server: {}", e.getMessage());
+        }
+    }
+
+    /**
+     * Sends a command to the Minecraft server.
+     *
+     * @param command The command to send.
+     */
+    public void sendCommandToServer(String[] command) {
+        if (minecraftServerProcess != null && minecraftServerProcess.isAlive()) {
+            serverCommandWriter.println(String.join(" ", command));
+            serverCommandWriter.flush();
+        } else {
+            this.main.getLogger().error("Minecraft server is not running");
+        }
+    }
+}
